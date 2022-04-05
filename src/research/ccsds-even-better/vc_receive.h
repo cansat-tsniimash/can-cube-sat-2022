8b3b1e45df893a3f545f4ff231add0d6db2af697
SeresHotes <sereshotes@gmail.com>
2022-04-05 22:13:07 +0300

    Попытки доделать распаковку фреймов

--- src/research/ccsds-even-better/vc_receive.h
+++ src/research/ccsds-even-better/vc_receive.h
@@ -1,20 +1,17 @@
 #pragma once
 
 #include "recieve.h"
+#include "fifo.h"
 
-typedef struct {
-    upid_t upid;
-    int map_id;
-    int pvn;
-} map_receive_t;
  
 typedef struct {
+    struct {
+        upid_t upid;
+        int map_id;
+        int pvn;
+    } mapr;
     cop_enum_t cop_type;
-    map_receive_t* mapr_list;
-    size_t mapr_count;
-    size_t mapr_list_size;
-    buffer_t buffer;
-    bool is_buffer_finished;
+    fifo_t fifo;
 } vc_receive_t;
 
 bool farm_1_process_frame(const map_data_t* md, const vc_data_t* vd) {
@@ -26,25 +23,27 @@ int get_pvn(uint8_t* data, size_t size) {
 
 }
 
-void map_receive_octet(vc_receive_t* vcr, const map_data_t* md) {
+bool map_receive_octet(vc_receive_t* vcr, const map_data_t* md) {
     
+    return true;
 }
 
-void map_receive_mapp(vc_receive_t* vcr, const map_data_t* md) {
+bool map_receive_mapp(vc_receive_t* vcr, const map_data_t* md) {
     
+    return true;
 }
 
 bool map_receive_mapa(vc_receive_t* vcr, const map_data_t* md) {
     size_t size = md->tfdf.size;
     if (md->tfdf.pointer_fh_lo != 0xFFFF) {
         size = md->tfdf.pointer_fh_lo;
-        vcr->is_buffer_finished = true;
     }
-    if (vcr->buffer.capacity - vcr->buffer.size < size) {
+    if (fifo_free_space(&vcr->fifo) < size + sizeof(size)) {
         return false;
     }
-    memcpy(vcr->buffer.data + vcr->buffer.size, md->tfdf.tfdz, size);
-    vcr->buffer.size += size;
+    uint8_t* arr = (uint8_t*)&size;
+    fifo_push_arr(&vcr->fifo, arr, sizeof(size));
+    fifo_push_arr(&vcr->fifo, md->tfdf.tfdz, size);
     return true;
 }
 
@@ -84,24 +83,18 @@ void vc_receive_cb(void* arg, const map_data_t* md, const vc_data_t* vd) {
         return;
     }
     
-    bool test = false;
-    for (size_t i = 0; i < vcr->mapr_count; i++) {
-        map_receive_t* cur = &vcr->mapr_list[i];
-        if (md->tfdf.upid == cur->upid) {
-            if (cur->upid == UPID_SP_OR_EP && 
-                cur->pvn == get_pvn(md->tfdf.tfdz, md->tfdf.size)) {
-                map_receive_mapp(vcr, md); 
-                test = true;
-            } else if (cur->upid == UPID_OCTET) {
-                map_receive_octet(vcr, md); 
-                test = true;
-            } else if (cur->upid == UPID_MAPA) {
-                map_receive_mapa(vcr, md); 
-                test = true;
-            }
+    bool map_received = false;
+    if (md->tfdf.upid == vcr->mapr.upid && md->tfdf.map_id == vcr->mapr.map_id) {
+        if (md->tfdf.upid == UPID_SP_OR_EP) {
+            map_received = map_receive_mapp(vcr, md); 
+        } else if (md->tfdf.upid == UPID_OCTET) {
+            map_received = map_receive_octet(vcr, md); 
+        } else if (md->tfdf.upid == UPID_MAPA) {
+            map_received = map_receive_mapa(vcr, md); 
         }
     }
-    if (!test) {
+    
+    if (!map_received) {
         //TODO: Send warning about unreceived map data
     }
     
